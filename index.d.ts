58cacc7a6a2e21c370feca2fbbf44648b54d9a6a
lizhooh <960316408@qq.com>
2018-07-18 23:15:35 +0800

    update: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,43 @@
 
-class ModuleBase {
+declare class ModuleBase {
     constructor(name: string);
-    get store(): Object;
-    get state(): Object;
+    /** 初始化的状态数据 */
+    initState: [any] | object | any;
+    /** 获取数据源 */
+    get store(): object;
+    /** 获取本模块的状态数据 */
+    get state(): object;
+    /** 提交一个数据改变请求 */
     commit(cb: (state) => {});
-    commit(cb: (state) => {}, cb: (newState) => {});
+    commit(cb1: (state) => {}, cb2: (newState) => {});
     commit(name: string, cb: (state) => {});
-    commit(name: string, cb: (state) => {}, cb: (newState) => {});
+    commit(name: string, cb1: (state) => {}, cb2: (newState) => {});
+    /** 返回对象白名单 */
     only(obj: object, keys: string);
 }
 
 interface Opts {
+    /**
+     * 是否开启 redux devtool 工具
+     */
     devtool?: boolean,
-    middlewares?: [],
+    /**
+     * redux 中间件设置
+     */
+    middlewares?: [any],
+    /**
+     * 对 Module 基类插入 mixin 函数
+     */
+    moduleMixin: object,
 }
 
-export default {
-    modules: (name: string, module: any) => any,
-    store: (initState: any, middlewares: []) => object,
-    actions: (name: string) => object,
-    Module: ModuleBase,
-    config: (options: Opts) => any,
-}
-
+/** 加载模块类 */
+export function modules(name: string, module: any): void;
+/** 返回一个创建后的 store 对象 */
+export function store(initState: any, middlewares: [any]): object;
+/** 返回模块所有的 action 函数 */
+export function actions(name: string): object;
+/** 配置项 */
+export function config(options: Opts): any;
+/** 模块基类，需要继承使用 */
+export const Module: ModuleBase;
