8c51603e01beb8fec64bb4c1e3dd981877d63d1b
lizhooh <960316408@qq.com>
2018-08-15 14:53:00 +0800

    update: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -55,17 +55,15 @@ interface Opts {
      * redux 中间件设置
      */
     middlewares?: [any],
-    /**
-     * 对 Module 基类插入 mixin 函数
-     */
-    moduleMixin: object,
 }
 
 /** 加载模块类 */
-export function modules(name: string, module: any): void;
+export function module(name: string, module: any): void;
 /** 返回一个创建后的 store 对象 */
 export function store(initState: any, middlewares: [any]): object;
 /** 返回模块所有的 action 函数 */
-export function actions(name: string): object;
+export function action(name: string): object;
+/** 对 Module 添加实例方法 */
+export function mixin(key: string, value: any): object;
 /** 配置项 */
 export function config(options: Opts): any;
