efd66b834fb9c9ed1e7f8ad9c6a16da0a46e6f8c
lizhooh <960316408@qq.com>
2018-09-11 17:44:16 +0800

    update: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
 
 interface IApp {
-    action: object,
-    module: object,
-    mixin: object,
+    action: object | any,
+    module: object | any,
+    mixin: object | any,
 }
 
 /** 模块基类，需要继承使用 */
 export class Module {
     constructor(name: string);
     /** 初始化的状态数据 */
-    get initState(): [any] | object | any;
+    initState: [any] | object | any;
     /** 获取数据源 */
-    get store(): object;
+    get store(): object | any;
     /** 获取本模块的状态数据 */
-    get state(): object;
+    get state(): object | any;
     /* 获取注入的 mixin */
-    get helper(): object;
+    get helper(): object | any;
 
     /**
      * 在 Module 构建完成后自动调用，此时可以获取 this.app
@@ -26,36 +26,37 @@ export class Module {
      * 提交一个数据改变请求
      * @params{Function}: cb - 执行函数
      */
-    commit(cb: (state) => {});
+    commit(cb: (state?: any) => any);
     /**
      * 提交一个数据改变请求
      * @params{Function}: cb1 - 执行函数
      * @params{Function}: cb2 - 新状态改变后回调
      */
-    commit(cb1: (state) => {}, cb2: (newState) => {});
+    commit(cb1: (state?: any) => any, cb2: (newState?: any) => any);
     /**
      * 提交一个数据改变请求
      * @params{String}: name - 模块名称
      * @params{Function}: cb - 执行函数
      */
-    commit(name: string, cb: (state) => {});
+    commit(name: string, cb: (state?: any) => any);
     /**
      * 提交一个数据改变请求
      * @params{String}: name - 模块名称
      * @params{Function}: cb1 - 执行函数
      * @params{Function}: cb2 - 新状态改变后回调
      */
-    commit(name: string, cb1: (state) => {}, cb2: (newState) => {});
+    commit(name: string, cb1: (state?: any) => any, cb2: (newState?: any) => any);
 
     /**
      * 提交一个数据改变请求，参数数据会被合并到 state 里。
      * @params{Object}: newState - 新的状态
      */
-    commitAssign(updateState);
-    commitAssign(updateState, cb: (state) => {});
+    commitAssign(updateState: any);
+    commitAssign(updateState: any, cb: (state?: any) => any);
 
     /* 应用的上下文 */
     app: IApp;
+
 }
 
 interface Opts {
@@ -72,31 +73,31 @@ interface Opts {
 /**
  * 加载模块类
  */
-export function module(name: string, module: any): void;
+export function module(name: string, module: any): object | any;
 
 /**
  * 返回一个创建后的 store 对象
  */
-export function store(initState: any, middlewares: [any]): object;
+export function store(initState: any, middlewares: [any]): object | any;
 
 /**
  * 返回模块所有的 action 函数
  */
-export function action(name: string): object;
+export function action(name: string): object | any;
 
 /**
  * 对 Module 注入属性，会被合并到 this 里
  */
-export function mixin(key: string, value: any): object;
+export function mixin(key: string, value: any)
 
 /**
  * 对 Module 添加辅助实例方法
  */
-export function helper(key: string, value: any): object;
+export function helper(key: string, value: any);
 /**
  * 配置项
  */
-export function config(options: Opts): any;
+export function config(options: Opts);
 
 /**
  * 获取所有的数据对象
